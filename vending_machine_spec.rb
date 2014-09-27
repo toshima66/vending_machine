b423e8b6f87f9207e4c7c4a99af732a5208e7c37
kytiken <kytiken@gmail.com>
2014-09-27 15:45:54 +0900

    商品を選択するメソッドのテストを追加

--- vending_machine_spec.rb
+++ vending_machine_spec.rb
@@ -1,9 +1,20 @@
 require './vending_machine'
 
 describe VendingMachine do
-  describe 'start' do
+
+  it(:vending_machine) { VendingMachine.new }
+
+  describe '商品を選択' do
+    describe '#select' do
+      it '0番目が選択された時はオレンジジュースを取得する' do
+        drink = vending_machine.select(0)
+        expect(drink).to eq()
+      end
+    end
   end
 
-  describe 'list' do
+  describe '金額を入力' do
+    describe '#put_coins' do
+    end
   end
 end
